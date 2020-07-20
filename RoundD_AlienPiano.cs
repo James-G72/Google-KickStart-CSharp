0e2aa93cf96358ac0089b4526f3fb5626aa9979b
James Gower <66065325+James-G72@users.noreply.github.com>
2020-07-20 12:45:37 +0100

    Create RoundD_AlienPiano.cs
    Initial attempt. I thought this problem would be a lot harder than it is but this simple approach works for the first case but WA for the second

---
+++ RoundD_AlienPiano.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Linq;
+using System.Collections.Generic; // Allows lists to be used. Important!
+
+namespace KickStart
+{
+    // Then we handle the rest of the logic in a seperate solutions class
+    class Solution
+    {
+
+        static void Main(string[] args)
+        {
+            int Cases = Int32.Parse(Console.ReadLine());
+            for (int case_ = 1; case_ < Cases + 1; case_++)
+            {
+                // The cases loop contains all solutions
+                int values = Int32.Parse(Console.ReadLine());
+                List<int> input_list = Console.ReadLine().Split(' ').Select(s => Convert.ToInt32(s)).ToList();
+                // Filter out values that appear more than once
+                List<int> filtered = input_list.Distinct().ToList();
+                int ups = 0;
+                int downs = 0;
+                int breaks = 0;
+                for (int i = 1; i < filtered.Count(); i++)
+                {
+                    if (filtered[i] > filtered[i - 1])
+                    {
+                        ups++;
+                        downs = 0;
+                    }
+                    else
+                    {
+                        downs++;
+                        ups = 0;
+                    }
+                    if (ups > 3 || downs > 3)
+                    {
+                        breaks++;
+                        ups = downs = 0;
+                    }
+                }
+
+                Console.WriteLine("Case #{0}: {1}", case_, breaks);
+            }
+        }
+    }
+}
